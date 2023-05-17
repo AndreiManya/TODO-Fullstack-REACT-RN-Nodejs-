79f0f501e03d3e3a4ef462a3ef82ca6577abda3c
AndreiManya <60035201+AndreiManya@users.noreply.github.com>
2023-05-17 16:15:12 +0300

    feat: add del button to component

--- mobile/components/item/item.tsx
+++ mobile/components/item/item.tsx
@@ -3,24 +3,61 @@ import {
   View,
   StyleSheet,
   Text,
+  Pressable,
+  GestureResponderEvent,
 } from 'react-native';
 
-type ItemProps = {value: string, checked: boolean};
+type ItemProps = {
+  value: string, 
+  checked: boolean, 
+  id: string,
+  remove: Function,
 
-const Item = ({value, checked}: ItemProps) => (
+};
+const Item = ({value, checked, id, remove}: ItemProps) => (
   <View style={{...styles.item, backgroundColor: checked ? 'green' : 'red'}}>
     <Text style={styles.value}>{value}</Text>
+    <Pressable 
+      style={styles.button} 
+      onPress={() => remove(id)}
+    >
+      <Text style={styles.text}>Del</Text>
+    </Pressable>
   </View>
 );
 
 const styles = StyleSheet.create({
   item: {
-    padding: 20,
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    paddingLeft: 15,
+    paddingRight: 5,
     marginVertical: 10,
     marginHorizontal: 10,
   },
   value: {
     fontSize: 20,
+    padding: 10,
+    flex: 1, 
+    flexWrap: 'wrap'
+  },
+  button: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    width: 45,
+    height: 30,
+    borderRadius: 4,
+    elevation: 3,
+    backgroundColor: 'gray',
+  },
+  text: {
+    fontSize: 16,
+    lineHeight: 21,
+    fontWeight: 'bold',
+    letterSpacing: 0.25,
+    color: 'black',
   },
 });
 
